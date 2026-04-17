ab3651e677688e150702f12b99840315e5c0d8c3
Greco42 <tatsuki29@gmail.com>
2026-04-17 10:47:36 +0900

    Delete js directory

--- js/loading.js
+++
@@ -1,5 +0,0 @@
-//logoの表示
-$(window).on('load',function(){
-  $("#loading").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
-  $("#loading_logo").delay(1500).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
-});
